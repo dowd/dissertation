ce8e5268391de132fbb1470f714d6081a772ade2
Dave Dowding <dowd@DowdMB.local>
2017-04-19 11:45:57 +0100

    Added trycatch for loading osm files as sp didn't work so going to try !((dim(osm_temp)[1]) next

--- Compare_OS_OSM.R
+++ Compare_OS_OSM.R
@@ -13,11 +13,20 @@ element_types=read.csv("elements.csv", header=FALSE, stringsAsFactors = FALSE)
 element_types=element_types[!element_types$V7=="",]
 
 #function to find distances between a point and multiple lines and returns a matrix of those distances
-distance2lines=function(os_point,osmlines) {
+min2points=function(os_point,osmpoints) {
+  distance_matrix=distm (osm_points,os_point)
+  result=c(min(distance_matrix),which.min(distance_matrix))
+  return(result)
+}
+
+
+#function to find distances between a point and multiple lines and returns a matrix of those distances
+min2lines=function(os_point,osmlines) {
   distance_matrix=matrix()
   for (line in 1:nrow(osm_lines)) {
     tryCatch({ distance_matrix[line]=dist2Line(os_point,osmlines[line,])[1] }, error=function(e){distance_matrix[line]=NA})}
-  return(distance_matrix)
+  result=c(min(distance_matrix, na.rm=TRUE),which.min(distance_matrix))
+  return(result)
 }
 
 #LOOP TO BE ADDED - go through city and elements
@@ -36,19 +45,37 @@ for (element_type in element_types[,1]) {
     #convert both files to spatial data frames
     coordinates(os_temp) <- cbind(os_temp$X , os_temp$Y)
     proj4string(os_temp)=CRS("+init=epsg:4326")
-    if(type=='point'|type=='polygon') {osm_points=as_sp(osm_temp, 'points')}
-    if(type=='line') {osm_lines=as_sp(osm_temp,'lines')
-    line_matrix=distance2lines(os_temp[1,],osm_lines)}
-    if(type=='polygon') {osm_polygons=as_sp(osm_temp,'polygons')
-    polygon_matrix=distance2lines(os_temp[1,],osm_polygons)}
+    os_point=os_temp[ents,]
+    tryCatch({
+      osm_points=as_sp(osm_temp, 'points')
+      result_points=min2points(os_point,osm_points)
+      }, warning=function(e){result_points=c(NA,NA)})
+    tryCatch({
+      osm_lines=as_sp(osm_temp,'lines')
+      result_lines=min2lines(os_point,osm_lines)
+      print("lines")
+      }, warning=function(e){result_lines=c(NA,NA)
+      print("lines warning")})
+    tryCatch({
+      osm_polygons=as_sp(osm_temp,'polygons')
+      result_polygons=min2lines(os_point,osm_polygons)
+      print("polygons")
+      }, warning=function(e){result_polygons=c(NA,NA)
+      print("polygons warning")})
+    
+    line_matrix=distance2lines(os_temp[1,],osm_lines)
+    
     
 }
 }
 
 
 
-
+osm_lines=as_sp(osm_temp,'lines')
+result_lines=min2lines(os_point,osm_lines)
+print("lines")
 
 #testing variables - only used to test thing working while creating project!!
 city="Cardiff"
 element_type="underpass"
+element_type="bus_station"
