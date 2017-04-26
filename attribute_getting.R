4a1783bc7f461beabae2c70c2133c0086c4435bd
Dave Dowding <dowd@dowd.in>
2017-04-26 21:56:00 +0100

    Added code to add data to dataframe using apply

--- attribute_getting.R
+++ attribute_getting.R
@@ -3,36 +3,33 @@ library(osmar)
 
 elements=read.csv("../data/results/element_attr_table.csv", header=TRUE, stringsAsFactors = FALSE)
 
+results = data.frame("City"=character(),"Element"=character(),"Type"=character(),"Key"=character(),"Value"=character(),"Total_OSM_elements"=numeric(),"No_source_tag"=numeric(), stringsAsFactors=FALSE)
+
 bbox = corner_bbox(-8, 50, 2, 60)
 src = osmsource_osmosis(file = "/Users/dowd/GIS_Data/easter/OSM_metro_extract/Manchester.osm")
 src2=osmsource_osmosis(file = "/Users/dowd/GIS_Data/easter/OSM_metro_extract/Cardiff.osm")
 #osm_temp = get_osm(bbox, src)
 src3=osmsource_osmosis(file = "/Users/dowd/GIS_Data/easter/OSM_metro_extract/Bristol.osm")
 #osm_temp = get_osm(bbox, src)
-osm_temp_c = get_osm(bbox, src2)
-osm_temp_b = get_osm(bbox, src3)
-
-summary(osm_temp)
-nrow(osm_temp$ways$attrs)
-
-subset=subset(osm_temp, node_ids = find(osm_temp, node(tags(k=="amenity" & v=="waste_basket"))))
+#osm_temp_c = get_osm(bbox, src2)
+#osm_temp_b = get_osm(bbox, src3)
 
 
-test=osm_temp$ways$tags$k=="highway" & osm_temp$ways$tags$v=="steps"
+number_elements=function(x) {
+  if (x[4]=="*") {
+  subset=subset(osm_temp, way_ids = find(osm_temp, way(tags(k==x[3]))))
+  subset2=subset(osm_temp, node_ids = find(osm_temp, node(tags(k==x[3]))))
+} else {
+  subset=subset(osm_temp, way_ids = find(osm_temp, way(tags(k==x[3] & v==x[4]))))
+  subset2=subset(osm_temp, node_ids = find(osm_temp, node(tags(k==x[3] & v==x[4]))))
+}
+  return(nrow(subset$ways$attrs)+nrow(subset2$nodes$attrs))
+}
 
-sum(osm_temp$relations$tags$k=="source")
-
-nrow(osm_temp$nodes$attrs)
 
 
-sum(subset$nodes$tags$k=="source")
 
-subset=subset(osm_temp, way_ids = find(osm_temp, way(tags(k=="tunnel"))))
-nrow(subset$ways)
-
-subset
-
-get_attribute_count = function(x, output) {
+get_attribute_count = function(x) {
   element_name = x[1]
   type=x[2]
   key = x[3]
@@ -73,15 +70,20 @@ get_attribute_count = function(x, output) {
   
  
     print(paste(element_name, type,key,value,total,sauce,x[5],attr_count,x[6],attr_count2,x[7],attr_count3, sep=","))
-    cat(paste("Manchester",element_name, type, key,value,total,sauce,x[5],attr_count,x[6],attr_count2,x[7],attr_count3, sep=","), file= output, append = T, fill = T)
+    #cat(paste("Manchester",element_name, type, key,value,total,sauce,x[5],attr_count,x[6],attr_count2,x[7],attr_count3, sep=","), file= output, append = T, fill = T)
   
+    this_result=c("Manchester",element_name, type, key,value,total,sauce)
+    results[nrow(results)+1,] = this_result
+    
+    
+    
 }
 
-apply(elements, 1, get_attribute_count, output = '../data/test/Manchester_final.csv')
+apply(elements, 1, get_attribute_count)
 
 
 
-
+results = data.frame("City"=character(),"Element"=character(),"Type"=character(),"Key"=character(),"Value"=character(),"Total_OSM_elements"=numeric(),"No_source_tag"=numeric(), stringsAsFactors=FALSE)
 
 
 
